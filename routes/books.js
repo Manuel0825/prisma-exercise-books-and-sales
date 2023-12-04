1efe8a47fd1107c2b75ec43e2b8f3b6dcdfa7b30
Manuel Vallejo <manuelvallejo@Manuels-MacBook-Pro.local>
2023-12-04 18:28:51 +0100

    DONE

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,76 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const prisma = require("../prisma");
 
-router.get('/', (req, res) => {})
+router.get("/", async (req, res) => {
+  try {
+    const Books = await prisma.libro.findMany();
+    res.json(Books);
+  } catch (error) {
+    res.json("Server error");
+  }
+});
+
+
+router.get("/author/:author", async (req, res) => {
+  const author = await prisma.libro.findMany({
+    where: {
+      Autor: req.params.author,
+    },
+  });
+  try {
+    res.json(author);
+  } catch (error) {
+    res.json("Server error");
+  }
+});
+
+router.get("/price/:price", async (req, res) => {
+  const price = await prisma.libro.findMany({
+    where: {
+      Precio: {
+        gte: Number(req.params.price),
+      },
+    },
+  });
+  try {
+    res.json(price);
+  } catch (error) {
+    res.json("Server error");
+  }
+});
+
+router.get("/with-sales", async (req, res) => {
+  try {
+    const combineTablesIsbn = await prisma.venta.findMany({
+      include: {
+       Libro: {
+        select: {
+          Autor: true,
+          Titulo: true,
+          Precio: true,
+        }
+       }
+      },
+    });
+    res.json(combineTablesIsbn);
+  } catch (error) {
+    res.json("Server Error");
+  }
+});
+
+router.get("/:isbn", async (req, res) => {
+  const bookIsbn = await prisma.libro.findUnique({
+    where: {
+      ISBN: req.params.isbn,
+    },
+  });
+  try {
+    res.json(bookIsbn);
+  } catch (error) {
+    res.json("Server error");
+  }
+});
+
 
 module.exports = router;
--- routes/sales.js
+++ routes/sales.js
@@ -1,6 +1,79 @@
 const express = require("express");
 const router = express.Router();
+const prisma = require('../prisma');
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+    try {
+      const allSales = await prisma.venta.findMany();
+      res.json(allSales);
+    } catch (error) {
+      res.json("Server error");
+    }
+  });
+
+  
+
+    router.get("/book/:isbn", async (req, res) => {
+        const salesIsbn = await prisma.venta.findMany({
+          where: {
+            ISBN: req.params.isbn
+          },
+        });
+        try {
+          res.json(salesIsbn);
+        } catch (error) {
+          res.json("Server error");
+        }
+      });
+
+      router.get("/date/:date", async (req, res) => {
+        const newDate = await prisma.venta.findMany({
+          where: {
+            Fecha_Venta: new Date(req.params.date)
+          },
+        });
+        try {
+          res.json(newDate);
+        } catch (error) {
+          res.json("Server error");
+        }
+      });
+      router.get("/sales/top", async (req, res) => {
+        try {
+          const combineTablesIsbn = await prisma.venta.findMany({
+            include: {
+             Libro: {
+              select: {
+                Autor: true,
+                Titulo: true,
+                Precio: true,
+              }
+             }
+            },
+          });
+          res.json(combineTablesIsbn);
+        } catch (error) {
+          res.json("Server Error");
+        }
+      });
+
+
+      
+      router.get("/:id", async (req, res) => {
+        const bookIsbn = await prisma.venta.findUnique({
+          where: {
+          ID_Venta: Number(req.params.id)
+          },
+          });
+          try{
+           res.json(bookIsbn)
+          }
+            catch (error) {
+              res.json("Server error");
+            }
+          
+        });
+
+
 
 module.exports = router;
